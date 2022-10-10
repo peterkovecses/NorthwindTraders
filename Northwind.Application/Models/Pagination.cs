26315c6a2650507722e38e16b54125c5207e79cc
Peter Kovecses <kovecsespeter@gmail.com>
2022-10-10 08:12:19 +0200

    Set valid pagination property values on initialization

--- Northwind.Application/Models/Pagination.cs
+++ Northwind.Application/Models/Pagination.cs
@@ -2,7 +2,20 @@
 {
     public class Pagination
     {
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 1;
+        private const int MinPageNumber = 1;
+        private const int MinPageSize = 1;
+        private int _pageNumber = MinPageNumber;
+        private int _pageSize = MinPageSize;
+
+        public int PageNumber 
+        { 
+            get => _pageNumber; 
+            set => _pageNumber = value < MinPageNumber ? MinPageNumber : value; 
+        }
+        public int PageSize 
+        { 
+            get => _pageSize; 
+            set => _pageSize = value < MinPageSize ? MinPageSize : value; 
+        }
     }
 }
